ed4dda6b9ba3682dfc57b814378955c93ab02a0b
Jessica G <gillan.jessica@gmail.com>
2017-02-11 19:16:26 -0700

    asset precompile config

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module RachioRun
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.assets.precompile += ['app.js']
   end
 end
